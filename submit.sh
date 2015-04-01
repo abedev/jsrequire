2fed378a379fe62320a52b782ef0e67298dd65a9
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:10:15 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm jsrequire.zip 2> /dev/null
-zip -r jsrequire.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md
+zip -r jsrequire.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit jsrequire.zip
